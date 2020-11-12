b94ac32b2f2f86c2fd621e4172c899317b806b51
SSUN2 <2210211@naver.com>
2020-11-13 01:50:49 +0900

    행맨완성!

--- HelloJava/src/array/Project15_hengman.java
+++ HelloJava/src/array/Project15_hengman.java
@@ -2,36 +2,47 @@ package array;
 
 import java.util.Scanner;
 
-public class Project15_hengman {
+public class Project15_hengman{
 
-	// 이번주까지 풀어서 내기 
-	// ----- ㅇㅖ상글자입력:
-	// a
-	// a---- 1번시도
-	
 	public static void main(String[] args) {
-		
+		// apple
+
 		Scanner sc = new Scanner(System.in);
-		String [] apple = "apple".split("");
-		String [] answer = {"_","_","_","_","_"};
-		
-		while(true) {
-			System.out.println("_____");
-			System.out.print("예상글자입력:");
+
+		String moonja = "apple";
+		String[] bae = moonja.split("");
+		String[] answer = { "_", "_", "_", "_", "_" };
+
+		int count = 0;
+		int sw = 0;
+		while (true) {
+			System.out.println(count+1+"번째 시도입니다.");
+			System.out.print("문자를 입력하세요 :");
+			
 			String alp = sc.nextLine();
-			for (int j = 0; j < answer.length; j++) {
-				if(apple[j].equals(alp)) {
-					answer[j] =alp;
-//					System.out.print(answer[i]);
-				
-			}
+			count++;
+			for (int i = 0; i < answer.length; i++) {
+				if(alp.equals(answer[i])) {
+					System.out.println("이미있는글씨");
+					break;
+				} else if (bae[i].equals(alp)) {
+					answer[i] = alp;
+					sw++;
 				}
 			}
+			for (int i = 0; i < answer.length; i++) {
+				System.out.print(answer[i]);
+			}
+			if (sw == answer.length) {
+				System.out.println("==정답입니다==");
+				break;
+			}
+			if (count == 13) {
+				System.out.println("입력횟수 초과");
+				break;
+			}
+
 		}
-		
-		
-		
-
 	}
-
 }
+
